d6132bc910ddc4eba1e0327582ca85dad8a6e9de
Jeniffer Maximo <jeniffer1994maximo@gmail.com>
2026-02-02 11:52:50 -0300

    Adiciona seção de serviços disponíveis e ajustes no site

--- app.py
+++ app.py
@@ -61,6 +61,6 @@ def simulacao():
             'message': 'Erro ao calcular simulação.'
         }), 500
 
-if __name__ == '__main__':
-    app.run()
 
+if __name__ == "__main__":
+    app.run(debug=True)
